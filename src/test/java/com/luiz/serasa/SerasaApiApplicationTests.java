b24e48c5ef1664c7a6f568646d3e9bdd1adaee19
luiz.coelho <uizhenrique@gmail.com>
2025-01-20 18:01:09 -0400

    adicionados arquivos iniciais do projeto baseado no spring initializr

---
+++ src/test/java/com/luiz/serasa/SerasaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.serasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerasaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
