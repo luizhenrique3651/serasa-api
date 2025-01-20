8171cec8e797c70173f124e438557517d512b864
luiz.coelho <uizhenrique@gmail.com>
2025-01-20 19:04:42 -0400

    Corrigida ausencia de anotações na entidade de Pessoa

--- src/main/java/com/luiz/serasa/entity/Pessoa.java
+++ src/main/java/com/luiz/serasa/entity/Pessoa.java
@@ -1,30 +1,33 @@
 package com.luiz.serasa.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Pessoa {
-
+	
+	@Id
 	Long id;
-	
+	@Column
 	String nome;
-	
+	@Column
 	Integer idade;
-	
+	@Column
 	String cep;
-	
+	@Column
 	String estado;
-	
+	@Column
 	String cidade;
-	
+	@Column
 	String bairro;
-	
+	@Column
 	String logradouro;
-	
+	@Column
 	String telefone;
-	
+	@Column
 	Integer score;
 	
 }
