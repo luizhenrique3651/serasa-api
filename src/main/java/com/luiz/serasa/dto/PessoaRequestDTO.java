bf780c08b89b36dc639cdef0aa24c22db866f0a5
luiz.coelho <uizhenrique@gmail.com>
2025-01-21 17:48:25 -0400

    Criados construtores para geração de um DTO a partir de um Objeto de Pessoa

--- src/main/java/com/luiz/serasa/dto/PessoaRequestDTO.java
+++ src/main/java/com/luiz/serasa/dto/PessoaRequestDTO.java
@@ -1,5 +1,11 @@
 package com.luiz.serasa.dto;
 
+import com.luiz.serasa.entity.Pessoa;
+
 public record PessoaRequestDTO(String nome, Integer idade, String cep, String telefone, Integer score) {
 
+	public PessoaRequestDTO(Pessoa pessoa) {
+		this(pessoa.getNome(), pessoa.getIdade(), pessoa.getCep(),  pessoa.getTelefone(),
+				pessoa.getScore());
+	}
 }
--- src/main/java/com/luiz/serasa/dto/PessoaResponseDTO.java
+++ src/main/java/com/luiz/serasa/dto/PessoaResponseDTO.java
@@ -3,7 +3,7 @@ package com.luiz.serasa.dto;
 import com.luiz.serasa.entity.Pessoa;
 
 public record PessoaResponseDTO(Long id, String nome, Integer idade, String cep, String estado, String cidade,
-		String bairro, String logradouro, String telefone, Integer scoreaz) {
+		String bairro, String logradouro, String telefone, Integer score) {
 
 	public PessoaResponseDTO(Pessoa pessoa) {
 		this(pessoa.getId(), pessoa.getNome(), pessoa.getIdade(), pessoa.getCep(), pessoa.getEstado(),
