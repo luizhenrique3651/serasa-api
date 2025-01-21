060b3619bdd40b0d4ffa6444211bc52303510e67
luiz.coelho <uizhenrique@gmail.com>
2025-01-21 17:30:42 -0400

    DTO's de requisição e resposta criados

---
+++ src/main/java/com/luiz/serasa/dto/PessoaRequestDTO.java
@@ -0,0 +1,5 @@
+package com.luiz.serasa.dto;
+
+public record PessoaRequestDTO(String nome, Integer idade, String cep, String telefone, Integer score) {
+
+}
---
+++ src/main/java/com/luiz/serasa/dto/PessoaResponseDTO.java
@@ -0,0 +1,13 @@
+package com.luiz.serasa.dto;
+
+import com.luiz.serasa.entity.Pessoa;
+
+public record PessoaResponseDTO(Long id, String nome, Integer idade, String cep, String estado, String cidade,
+		String bairro, String logradouro, String telefone, Integer scoreaz) {
+
+	public PessoaResponseDTO(Pessoa pessoa) {
+		this(pessoa.getId(), pessoa.getNome(), pessoa.getIdade(), pessoa.getCep(), pessoa.getEstado(),
+				pessoa.getCidade(), pessoa.getBairro(), pessoa.getLogradouro(), pessoa.getTelefone(),
+				pessoa.getScore());
+	}
+}
