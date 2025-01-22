a84206294912c80b0d3176edac4d702baef32de0
luiz.coelho <uizhenrique@gmail.com>
2025-01-22 19:44:39 -0400

    DTO's de login e Token

---
+++ src/main/java/com/luiz/serasa/dto/JwtResponseDTO.java
@@ -0,0 +1,3 @@
+package com.luiz.serasa.dto;
+
+public record JwtResponseDTO(String token) {}
---
+++ src/main/java/com/luiz/serasa/dto/LoginRequestDTO.java
@@ -0,0 +1,3 @@
+package com.luiz.serasa.dto;
+
+public record LoginRequestDTO(String username, String password) {}
